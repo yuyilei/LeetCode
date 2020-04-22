a2c108443e8f786aa7e2d10178db7c7676b0e118
yuyilei <yyl1352091742@gmail.com>
2020-04-22 17:18:12 +0800

    dp

---
+++ Dynamic-Programming/1359_Count-All-Valid-Pickup-and-Delivery-Options.cpp
@@ -0,0 +1,50 @@
+/*
+Given n orders, each order consist in pickup and delivery services. 
+
+Count all valid pickup/delivery possible sequences such that delivery(i) is always after of pickup(i). 
+
+Since the answer may be too large, return it modulo 10^9 + 7.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: 1
+Explanation: Unique order (P1, D1), Delivery 1 always is after of Pickup 1.
+Example 2:
+
+Input: n = 2
+Output: 6
+Explanation: All possible orders:
+(P1,P2,D1,D2), (P1,P2,D2,D1), (P1,D1,P2,D2), (P2,P1,D1,D2), (P2,P1,D2,D1) and (P2,D2,P1,D1).
+This is an invalid order (P1,D2,P2,D1) because Pickup 2 is after of Delivery 2.
+Example 3:
+
+Input: n = 3
+Output: 90
+ 
+
+Constraints:
+
+1 <= n <= 500
+
+*/
+
+/*
+ * 插空法
+ */
+class Solution {
+public:
+    int countOrders(int n) {
+        if (n == 1)
+            return 1;
+        vector<int> dp (n+1, 0);
+        dp[1] = 1;
+        const int mod = 1000000007;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = ((long)dp[i-1]*i*(2*i-1))%mod;
+        }
+        return dp[n];
+    }
+};
