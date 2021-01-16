41902226a70ffe7fc0db24fc5e45413239120b1e
yuyilei <yyl1352091742@gmail.com>
2021-01-16 18:51:12 +0800

    223 week

---
+++ conTest/223-week/1721_Swapping-Nodes-in-a-Linked-List.cpp
@@ -0,0 +1,70 @@
+/*
+You are given the head of a linked list, and an integer k.
+
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [1,4,3,2,5]
+Example 2:
+
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,8,7,3,0,9,5]
+Example 3:
+
+Input: head = [1], k = 1
+Output: [1]
+Example 4:
+
+Input: head = [1,2], k = 1
+Output: [2,1]
+Example 5:
+
+Input: head = [1,2,3], k = 2
+Output: [1,2,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= k <= n <= 105
+0 <= Node.val <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/swapping-nodes-in-a-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode *fast = head;
+        for (int i = 0; i < k- 1; i++) {
+            fast = fast->next;
+        }
+        ListNode *endk = head;
+        ListNode *slow = fast;
+        ListNode *pre = endk;
+        while (fast != NULL) {
+            fast = fast->next;
+            pre = endk;
+            endk = endk->next;
+        }
+        swap(slow->val, pre->val);
+        return head;
+    }
+};
